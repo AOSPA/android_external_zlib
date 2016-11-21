928f0445c4f1fc93478ba7add105aa372011dbbe
Alex Naidis <alex.naidis@linux.com>
2016-11-21 22:27:02 +0100

    zlib: regenerate Android specific headers
    Change-Id: I677d9e799415dd65ad1f5e0647891f8b40986bb5
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- src/inflate.h
+++ src/inflate.h
@@ -18,7 +18,7 @@
 
 /* Possible inflate modes between inflate() calls */
 typedef enum {
-    HEAD,       /* i: waiting for magic header */
+    HEAD = 16180,   /* i: waiting for magic header */
     FLAGS,      /* i: waiting for method and flags (gzip) */
     TIME,       /* i: waiting for modification time (gzip) */
     OS,         /* i: waiting for extra flags and operating system (gzip) */
