madler/zlib

commit f1ebdd6a9c495a5db9a22aa80dd7d54ae7db42e9

Permit stronger flushes after Z_BLOCK flushes.
The incorporation of the Z_BLOCK flush did not update the rejection
of lower ranked flushes immediately after higher ranked flushes with
no more input data.  This prevented an empty Z_SYNC_FLUSH right after
a Z_BLOCK flush, which would be desired to bring the deflate stream
to a byte boundary conditionally on whether or not it was already at
a byte boundary.  This patch re-ranks Z_BLOCK above Z_NO_FLUSH but
below Z_PARTIAL_FLUSH, allowing stronger empty flushes to follow a
Z_BLOCK flush.

deflate.c
@@ -155,6 +155,9 @@ local const config configuration_table[10] = {
 struct static_tree_desc_s {int dummy;}; /* for buggy compilers */
 #endif
 
+/* rank Z_BLOCK between Z_NO_FLUSH and Z_PARTIAL_FLUSH */
+#define RANK(f) (((f) << 1) - ((f) > 4 ? 9 : 0))
+
 /* ===========================================================================
  * Update a hash value with the given input byte
  * IN  assertion: all calls to to UPDATE_HASH are made with consecutive
@@ -858,7 +861,7 @@ int ZEXPORT deflate (strm, flush)
      * flushes. For repeated and useless calls with Z_FINISH, we keep
      * returning Z_STREAM_END instead of Z_BUF_ERROR.
      */
-    } else if (strm->avail_in == 0 && flush <= old_flush &&
+    } else if (strm->avail_in == 0 && RANK(flush) <= RANK(old_flush) &&
                flush != Z_FINISH) {
         ERR_RETURN(strm, Z_BUF_ERROR);
     }

AOSPA/android_external_zlib

commit 35565e26b1455fecc779ee2fecb3d2676694e2d3

zlib: Degrade Z_BLOCK only when last flush was Z_BLOCK
This fixes regression introduced by f1ebdd6a9c495a5db9a22aa80dd7d54ae7db42e9.
(Permit stronger flushes after Z_BLOCK flushes).

Now this code is valid again:
deflate(stream, Z_SYNC_FLUSH);
deflateParams(stream, newLevel, Z_DEFAULT_STRATEGY);

Change-Id: Ibcf8c0801c1ae8e40fcb2cd72b0324b4cb2ed5d3
Signed-off-by: Adam Tkac <atkac@redhat.com>
Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

deflate.c
@@ -898,9 +898,16 @@ int ZEXPORT deflate (strm, flush)
      * flushes. For repeated and useless calls with Z_FINISH, we keep
      * returning Z_STREAM_END instead of Z_BUF_ERROR.
      */
-    } else if (strm->avail_in == 0 && RANK(flush) <= RANK(old_flush) &&
-               flush != Z_FINISH) {
-        ERR_RETURN(strm, Z_BUF_ERROR);
+    } else if (strm->avail_in == 0 && flush != Z_FINISH) {
+        char err;
+
+        /* Degrade Z_BLOCK only when last flush was Z_BLOCK */
+        err = (old_flush == Z_BLOCK) ?
+              RANK(flush) <= RANK(old_flush) : flush <= old_flush;
+
+        if (err) {
+            ERR_RETURN(strm, Z_BUF_ERROR);
+        }
     }
 
     /* User must not provide more input after the first FINISH: */